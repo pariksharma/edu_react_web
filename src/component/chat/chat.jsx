33983d6b98909524a17b32e206c95ae8d9566677
Parikshit <parikshit.sharma1@appsquadz.com>
2024-12-03 11:36:16 +0530

    UI Live Chat

--- src/component/chat/chat.jsx
+++ src/component/chat/chat.jsx
@@ -199,12 +199,13 @@ const Chat = ({chat_node, course_id, video_id}) => {
                       </div>
                       <h4 className="m-0 pdf_title flex-fill">{pdf?.pdf_title}</h4>
                       <svg
+                        style={{color: "#A3A3A3"}}
                         stroke="currentColor"
                         fill="currentColor"
                         strokeWidth="0"
                         viewBox="0 0 512 512"
-                        height="1em"
-                        width="1em"
+                        height="16"
+                        width="24"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path>
--- src/component/chat/liveChat.jsx
+++ src/component/chat/liveChat.jsx
@@ -15,6 +15,7 @@ import { MdAudioFile } from "react-icons/md";
 import { MdOutlinePictureAsPdf } from "react-icons/md";
 import * as pdfjs from "pdfjs-dist";
 
+
 // Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB8ISZRq949XJrbNeZm0gK54d9Q3zAzBtI",
@@ -511,6 +512,8 @@ const LiveChat = ({ chat_node, course_id, isPublic }) => {
     }
   };
 
+  // Toggle the dropdown visibility
+
   // const onEmojiClick = (emojiObject) => {
   //   console.log('Selected Emoji:', emojiObject.emoji); // Logs the emoji character
   //   setInput((prevInput) => prevInput + emojiObject.emoji);
@@ -605,6 +608,12 @@ const LiveChat = ({ chat_node, course_id, isPublic }) => {
                                 {/* {" "}|{" "} */}
                                 {/* <i class="bi bi-three-dots-vertical"></i> */}
                               </small>
+                                
+                              {/* <div class="dropdown-content">
+                                <a href="#"><i class="bi bi-pin-angle"></i> Pin Chat</a>
+                                  <hr class="divider" />
+                                <a href="#"><i class="bi bi-trash3"></i> Delete</a>
+                              </div> */}
                             </div>
                           </div>
                         </div>

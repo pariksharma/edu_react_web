cafb6a4348b17f2074f948abc2869d32772ce92e
Parikshit <parikshit.sharma1@appsquadz.com>
2024-12-09 15:05:30 +0530

    poll issue solved

--- src/component/chat/MQTTLivePollOptions.jsx
+++ src/component/chat/MQTTLivePollOptions.jsx
@@ -1,82 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { format } from "date-fns";
-// import { TiInputChecked } from "react-icons/ti";
-import { get, onValue, ref } from 'firebase/database';
+import React, { useState } from 'react'
 
-const LivePollOptions = ({poll, renderCountdown, index, handleSubmitAnswer, database, chat_node, getFireBaseKey}) => {
+const MQTTLivePollOptions = ({poll, index, renderCountdown}) => {
 
     const [selectedOptions, setSelectedOptions] = useState({})
     const [alreadySubmitted, setAlreadySubmitted] = useState(false);
     const [selectAnswer, setSelectAnswer] = useState(false)
     const [myAnswer, setMyAnswer] = useState('')
-    const [answerSelect, setAnswerSelect] = useState(0)
 
-    useEffect(() => {
-      const fetchAlreadySubmit = async () => {
-        const result = await alreadySubmit();
-        setAlreadySubmitted(result); // Update the state with the result
-      };
-  
-      fetchAlreadySubmit();
-    }, [poll]);
-
-    const formatTime = (date) => {
-        // console.log('data', date);
-          const cr_date = new Date(date);
-          if (cr_date) {
-            return format(cr_date, "h:mm a");
-          }
-        };
-
-        const handleOptionChange = (pollId, option) => {
-            // console.log('pollId, option', pollId, option)
-            let selectedAnswer = "";
-            if(option == "option_1") {
-                selectedAnswer = '1'
-                setAnswerSelect(1)
-            }
-            else if(option == "option_2") {
-                selectedAnswer = '2'
-                setAnswerSelect(2)
-            }
-            else if(option == "option_3") {
-                selectedAnswer = '3'
-                setAnswerSelect(3)
-            }
-            else if(option == "option_4") {
-                selectedAnswer = '4'
-                setAnswerSelect(4)
-            }
-            setSelectAnswer(true)
-            handleSubmitAnswer(poll, selectedAnswer)
-
-            setSelectedOptions((prev) => ({
-              ...prev,
-              [pollId]: option, // Update the selected option for the specific poll
-            }));
-          };
-
-          const alreadySubmit = async () => {
-            try {
-              const appId = localStorage.getItem("appId"); 
-              const userId = localStorage.getItem("user_id"); 
-              const userName = localStorage.getItem("userName");
-              const firebaseId = getFireBaseKey(poll.id)
-              // console.log('firebaseId', firebaseId)
-              const alreadySubmitRef = ref(
-                database,
-                `${appId}/chat_master/${chat_node}/Poll/${firebaseId}/users/${userId}`
-              );
-              const snapshot = await get(alreadySubmitRef); // 
-              const alreadySubmitValue = snapshot.val();
-                // console.log('alreadySubmitValue', alreadySubmitValue)
-              setMyAnswer(alreadySubmitValue)
-              return !!alreadySubmitValue;
-            } catch (error) {
-              console.log('error found', error)
-            }
-          }
-
+    console.log('poll', poll)
 
   return (
     <div className="card mb-2" style={{width: "100%"}} key={index}>
@@ -88,7 +19,8 @@ const LivePollOptions = ({poll, renderCountdown, index, handleSubmitAnswer, data
         <div className="hl_timer">
           <p className="m-0 d-flex l_timer">
             <i className="bi bi-clock-history me-1"></i>{" "}
-            <span className="history_timer m-0">{renderCountdown(poll?.valid_till)}</span>
+            {/* <span className="history_timer m-0">{renderCountdown(poll?.valid_till)}</span> */}
+            <span className="history_timer m-0">{'00:15'}</span>
           </p>
         </div>
         <button className="leader-btn" style={{display:"none"}}>
@@ -98,6 +30,7 @@ const LivePollOptions = ({poll, renderCountdown, index, handleSubmitAnswer, data
       <div className='card-body'>
         {['option_1', 'option_2', 'option_3', 'option_4'].map((optionKey, idx) => {
             const optionValue = poll[optionKey];
+            {console.log('optionValue', optionValue)}
             if (!optionValue) return null; // Skip empty options
 
             const attemptKey = `attempt_${idx + 1}`;
@@ -192,15 +125,10 @@ const LivePollOptions = ({poll, renderCountdown, index, handleSubmitAnswer, data
                 <div className="progress">
                   <div
                     className={
-                      alreadySubmitted ? 
-                        parseInt(poll?.answer) == idx + 1
-                          ? `progress-bar-1`
-                          : answerSelect == idx + 1 ? `progress-bar-fail` : ''
-                      :
-                      selectAnswer &&
-                          parseInt(poll?.answer) == idx + 1
-                            ? `progress-bar-correct`
-                            : answerSelect == idx + 1 ? `progress-bar-fail` : ''
+                      (alreadySubmitted || selectAnswer) &&
+                      parseInt(poll?.answer) == idx + 1
+                        ? `progress-bar-1`
+                        : ``
                     }
                     style={{
                       width: `${
@@ -223,4 +151,4 @@ const LivePollOptions = ({poll, renderCountdown, index, handleSubmitAnswer, data
   )
 }
 
-export default LivePollOptions
+export default MQTTLivePollOptions
